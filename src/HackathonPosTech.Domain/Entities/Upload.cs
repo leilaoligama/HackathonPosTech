e3fea024e87c6cc04afe4563e190f18875ef1078
Leila <leilagama@gmail.com>
2024-03-16 11:40:51 -0300

    Manutenção entidade Upload.

---
+++ src/HackathonPosTech.Domain/Entities/Upload.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackathonPosTech.Domain.Entities
+{
+    public class Upload
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string Nome { get; set; }
+        public string CaminhoUpload { get; set; }
+        public string CaminhoZip { get; set; }
+    }
+}
