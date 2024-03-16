f49516b982bf0180a11eb806e75b1ba82636ca77
Leila <leilagama@gmail.com>
2024-03-16 09:52:20 -0300

    Add project files.

---
+++ HackathonPosTech/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HackathonPosTech.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
